b14d65b6a9b4c2a53e73fcc2a7ed380540f07a7c
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-11-10 13:37:32 -0300

    criar conta no site usando firebase

---
+++ js/register.js
@@ -0,0 +1,19 @@
+var inputEmail = document.getElementById('InputEmail');
+var inputPassword = document.getElementById('InputPassword');
+var btnRegister = document.getElementById('btnRegister');
+
+btnRegister.addEventListener('click', function(){
+    firebase.auth().createUserWithEmailAndPassword(inputEmail.value, inputPassword.value).then(function (result){
+        alert("Usuario Cadastrado com Sucesso!");
+        console.log("Sucesso");
+        window.location.replace('initial-page.html');
+
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+        console.log("Falha no cadastro!");
+      });
+});
