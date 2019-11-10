674f2e3bb8c958469a352823a55e00f31e21cd05
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-11-09 23:01:27 -0300

    redirecionamento de pagina no login

--- js/authentication.js
+++ js/authentication.js
@@ -7,6 +7,8 @@ btnLogin.addEventListener('click', function(){
         alert("Usuario Conectado!");
         console.log("Sucesso!");
 
+        window.location.replace('initial-page.html');  //Redireciona o usuario para a outra pagina
+
     }).catch(function(error) {   
         // Handle Errors here.
         var errorCode = error.code;
