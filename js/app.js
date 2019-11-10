bb9798b374df34d3b91c288a203b0041ba334ecb
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-11-09 22:28:22 -0300

    função de autenticação com usuario

---
+++ js/app.js
@@ -0,0 +1,14 @@
+  // Configuração do firebase no aplicativo
+  var firebaseConfig = {
+    apiKey: "AIzaSyDTfswC-qMI12DU9RM-zhpawBDk2ufjZXg",
+    authDomain: "agendajs-7dfce.firebaseapp.com",
+    databaseURL: "https://agendajs-7dfce.firebaseio.com",
+    projectId: "agendajs-7dfce",
+    storageBucket: "agendajs-7dfce.appspot.com",
+    messagingSenderId: "680453992482",
+    appId: "1:680453992482:web:eda19884d2180cd867da7b",
+    measurementId: "G-B4V0PPGTBM"
+  };
+  // Inicia o Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ js/authentication.js
@@ -0,0 +1,19 @@
+var btnLogin = document.getElementById('btnLogin');
+var inputEmail = document.getElementById('InputEmail');
+var inputPassword = document.getElementById('InputPassword');
+
+btnLogin.addEventListener('click', function(){
+    firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputPassword.value).then(function (result){  // .value vai capturar o valor dado ao elemento
+        alert("Usuario Conectado!");
+        console.log("Sucesso!");
+
+    }).catch(function(error) {   
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+
+        alert(errorMessage);
+        console.log("Falha na conexao!");
+      });
+});
