318e47f40b3aead26b26e47fc5f1e9556e3e3185
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-11-12 00:21:09 -0300

    ajustes na função de lembrar email

--- js/rememberPassword.js
+++ js/rememberPassword.js
@@ -1,21 +1,25 @@
-var checkboxPassword = document.getElementById('checkboxPassword');
+var checkboxPassword = document.getElementById('customCheck');
 var InputEmail = document.getElementById('InputEmail');
 var InputPassword = document.getElementById('InputPassword');
 
 if (localStorage.checkbox && localStorage.checkbox != "") {
     checkboxPassword.setAttribute("checked", "checked");
     InputEmail.value = localStorage.username;
+    //localStorage.password = InputPassword.value;
 } else {
     checkboxPassword.removeAttribute("checked");
     InputEmail.value = "";
+   //InputPassword.value = "";
 }
 
 function lsRememberMe() {
     if (checkboxPassword.checked && InputEmail.value != "") {
         localStorage.username = InputEmail.value;
+        //localStorage.password = InputPassword.value;
         localStorage.checkbox = checkboxPassword.value;
     } else {
         localStorage.username = "";
+        localStorage.password = "";
         localStorage.checkbox = "";
     }
 }
