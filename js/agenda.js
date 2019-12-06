18f41f82c2aacc35e13175997298834305b2b4b5
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-12-06 16:57:11 -0300

    Correções na agenda

--- js/agenda.js
+++ js/agenda.js
@@ -1,234 +1,87 @@
-var recordatoriosSlecionados = [];
+// Product Constructor
+class Product {
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
 
-//function deletar lembretes
+// UI Constructor
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Produto</strong>: ${product.name} -
+                    <strong>Preço</strong>: ${product.price} - 
+                    <strong>Ano</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete">Delete</a>
+                </div>
+            </div>
+        `;
+        productList.appendChild(element);
+    }
 
-function deletarRecordatorio() {
-    if (recordatoriosSlecionados.length > 0) {
-        var recordatorioExistentes = localStorage.getItem("recordatorios");
-        if (recordatorioExistentes == null || recordatorioExistentes == "" || typeof recordatorioExistentes == "undefined" || recordatorioExistentes == "undefined") {
-            var recordatoriosRecuperados = JSON.parse(recordatorioExistentes);
+    resetForm() {
+        document.getElementById('product-form').reset();
+    }
 
-            for (var i = 0; i < recordatoriosSlecionados.length; i++) {
-                for (var j = 0; j < recordatoriosSlecionados.length; j++) {
+    deleteProduct(element) {
+        if (element.name === 'delete') {
+            element.parentElement.parentElement.remove();
+            this.showMessage('Produto deletado com sucesso', 'success');
+        }
+    }
 
-                    if (recordatoriosSlecionados[i] == recordatoriosRecuperados[j].id) {
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-2`;
+        div.appendChild(document.createTextNode(message));
+        // Show in The DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        // Insert Message in the UI
+        container.insertBefore(div, app);
+        // Remove the Message after 3 seconds
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+}
 
-                        recordatoriosRecuperados[j].id = -1;
+// DOM Events
+document.getElementById('product-form')
+    .addEventListener('submit', function (e) {
 
-                    }
-                }
-            }
+        const name = document.getElementById('name').value,
+            price = document.getElementById('price').value,
+            year = document.getElementById('year').value;
 
-            var recordatorioTemporario = [];
-            for (var i = 0; i < recordatoriosRecuperados.length; i++) {
-                if (recordatoriosRecuperados[i].id != -1) {
-                    recordatorioTemporario.push(recordatoriosRecuperados[i]);
+        // Create a new Oject Product
+        const product = new Product(name, price, year);
 
+        // Create a new UI
+        const ui = new UI();
 
-                }
-            }
-
-            //deletar recordatorio
-
-            if (recordatorioTemporario.length == 0) {
-                localStorage.setItem("recodatorios", "");
-            } else {
-                saveRecordatorios(recordatorioTemporario);
-            }
-
-            mostrarRecordatorios();
-            selecionarRecordatorio();
-
-
+        // Input User Validation
+        if (name === '' || price === '' || year === '') {
+            ui.showMessage('Adicione dados em todos campos', 'danger');
         }
 
-    }
-}
+        // Save Product
+        ui.addProduct(product);
+        ui.showMessage('Produro adicionado com sucesso', 'success');
+        ui.resetForm();
 
+        e.preventDefault();
+    });
 
-
-//Comprovar se está funcionando
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Funciona!!");
-
-    // mostrarErros();
-
-    document.getElementById("buttomSave").onclick = criarLembrete;
-});
-
-//Function para verificar se tem texto
-function textoValido(texto) {
-    if (texto == null || texto == "" || texto.lenght < 1) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-//Function para mostrar erros
-function mostrarErros() {
-    var html = "";
-    html += '<div class="alert alert-danger" role="alert">';
-    html += 'Por favor, informe alguma coisa';
-    html += '</div>';
-
-    document.getElementById('erro').innerHTML = html;
-}
-
-//Function para limpar o erro
-
-function limparErros() {
-    document.getElementById('erro').innerHTML = "";
-}
-
-//Function para criar lembrete
-
-function criarLembrete() {
-    var conteudoTextArea = document.getElementById("texto").value;
-    if (!textoValido(conteudoTextArea)) {
-        mostrarErros();
-        return;
-    }
-    limparErros();
-
-    //Criar as variaveis para tempo
-    var referencia = new Date();
-    var id = referencia.getTime();
-    var data = referencia.toLocaleDateString();
-    var texto = conteudoTextArea;
-
-    //JSON = js objetos
-
-    var recordatorio = {
-        "id": id,
-        "data": data,
-        "texto": texto
-    };
-
-    //Function para comprovar se existe lembrete
-    comprovarRecordatorio(recordatorio);
-    document.getElementById("texto").value = "";
-}
-
-function recordatorioValido(recordatorioExistentes) {
-    if (recordatorioExistentes == null || recordatorioExistentes == "" || typeof recordatorioExistentes == "undefined" || recordatorioExistentes == "undefined") {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
-//Function para comprovar se existe lembrete
-function comprovarRecordatorio(recordatorio) {
-    var recordatorioExistentes = localStorage.getItem("recordatorios");
-    if (!recordatorioValido(recordatorioExistentes)) {
-        var recordatorios = [];
-        recordatorios.push(recordatorio);
-
-        //Salvar recordatorio
-        saveRecordatorios(recordatorios);
-    } else {
-        var recordatoriosRecuperados = JSON.parse(recordatorioExistentes);
-
-        //Salva recordatorio
-        recordatoriosRecuperados.push(recordatorio);
-        saveRecordatorios(recordatoriosRecuperados);
-    }
-    mostrarRecordatorios();
-}
-
-// Função para selecionar recordatorios...
-function selecionarRecordatorio() {
-    var recordatorios = document.getElementsByClassName("recordatorio");
-    for (var i = 0; i < recordatorios.length; i++) {
-        document.getElementById(recordatorios[i].id).onclick = function(e){
-            e.stopPropagation();
-            //Caso tenha recordatorio
-            if (recordatoriosSlecionados.indexOf(this.id) == -1) {
-                this.style.backgroundColor = "red";
-                recordatoriosSlecionados.push(this.id);
-            } else {
-                //Caso ñ tenha
-                this.style.backgroundColor = "#41dff4";
-                for (var b = 0; b < recordatoriosSlecionados.length; b++) {
-                    if (recordatoriosSlecionados[b] == this.id) {
-                        recordatoriosSlecionados[b] = 0;
-                    }
-
-                }
-            }
-
-            var recordatorioTemporario = [];
-            for (var j = 0; j < recordatoriosSlecionados.length; j++) {
-                if (recordatoriosSlecionados[j] != 0) {
-                    recordatorioTemporario.push(recordatoriosSlecionados[j]);
-                }
-
-            }
-
-            recordatoriosSlecionados = recordatorioTemporario;
-
-        };
-    }
-}
-
-
-//Function para salvar lembrete ou recordatorioo
-function saveRecordatorios(recordatorios) {
-    var recordatoriosJSON = JSON.stringify(recordatorios);
-    localStorage.setItem("recordatorios", recordatoriosJSON);
-}
-
-//Function para exibir os itens
-function mostrarRecordatorios() {
-    var html = "";
-
-    var recordatorioExistentes = localStorage.getItem("recordatorios");
-    if (!recordatorioValido(recordatorioExistentes)) {
-        html = "Não existe nenhum lembrete";
-        document.getElementById("recordatorios").innerHTML = html;
-    }
-    else {
-        recordatoriosRecuperados = JSON.parse(recordatorioExistentes);
-        for (var i = 0; i < recordatoriosRecuperados.length; i++) {
-            html += formatarRecordatorio(recordatoriosRecuperados[i]);
-        }
-        document.getElementById("recordatorios").innerHTML = html;
-    }
-}
-
-//Função para exibir os lembretes
-function formatarRecordatorio(recordatorio) {
-    var html = "";
-    html += '<div class="recordatorio" id="' + recordatorio.id + '">';
-    html += '<div class="row">';
-    html += '<div class="col-6 text-left">';
-    html += '<small><i class="fa fa-calendar-alt" aria-hidden="true"></i>' + recordatorio.data + '</small>';
-    html += '</div>';
-    html += '<div class="col-6 text-right">';
-    html += '<small><i class="fa fa-window-close" aria-hidden="true"></i></small>';
-    html += '</div>';
-    html += '</div>';
-    html += '<br>';
-    html += '<div class="row">';
-    html += '<div class="col-12">';
-    html += recordatorio.texto;
-    html += '</div>';
-    html += '</div>';
-    html += '</div>';
-    html += '<br>';
-
-    return html;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Funciona!!");
-
-    // mostrarErros();
-
-    document.getElementById("buttomSave").onclick = criarLembrete;
-    document.getElementById("buttomDelete").onclick = deletarRecordatorio;
-    mostrarRecordatorios();
-    selecionarRecordatorio();
-});
+document.getElementById('product-list')
+    .addEventListener('click', function (e) {
+        const ui = new UI();
+        ui.deleteProduct(e.target);
+        e.preventDefault();
+    });
